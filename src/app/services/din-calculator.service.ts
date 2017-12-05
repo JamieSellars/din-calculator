ea7f463c8e82711f93018835553df6c7cd0b850f
Jamie Sellars <jamie.sellars@perisher.com.au>
2017-12-06 09:55:47 +1100

    UPDATE DIN Values and Solelength array

--- src/app/services/din-calculator.service.ts
+++ src/app/services/din-calculator.service.ts
@@ -5,7 +5,7 @@ import { DINSetting } from '../models/dinSetting';
 export class DinCalculatorService {
 
   private _dinSettings: DINSetting;
-  private _dins: any[] = [];
+  private _dins= new Array<any[]>();
   
   private _skierCodes: any = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','0'];
 
@@ -66,52 +66,52 @@ export class DinCalculatorService {
   setDINs(){
 
     // A
-    this._dins.push([0.75,0.75,null,null,null,null]);
+    this._dins.push([0.75,  0.75, 0.75, null, null, null, null, null]);
 
     // B
-    this._dins.push([1,1,0.75,null,null,null]);
+    this._dins.push([1, 0.75, 0.75, 0.75, null, null, null, null]);
 
     // C 
-    this._dins.push([1.5,1.25,1,null,null,null]);
+    this._dins.push([1.5, 1.25, 1.25, 1, null, null, null, null]);
 
     // D
-    this._dins.push([1.75,1.5,1.5,1.25,null,null]);
+    this._dins.push([2, 1.75,  1.5,  1.5,  1.25, null, null, null]);
 
     // E
-    this._dins.push([2.25,2,1.75,1.5,1.5,null]);
+    this._dins.push([2.5, 2.25,  2,  1.75, 1.5,  1.25,  null, null]);
     
     // F 
-    this._dins.push([2.75,2.5,2.25,2,1.75,1.5]);
+    this._dins.push([3,  2.75,  2.5,  2.25, 2,  1.75, 1.75, null]);
 
     // G
-    this._dins.push([3.5,3,2.75,2.5,2.25,2]);    
+    this._dins.push([null, 3.5  ,3, 2.75, 2.5,  2.25, 2, null]);    
 
     // H 
-    this._dins.push([null,3.5,3,3,2.75,2.5]);    
+    this._dins.push([null,  null, 3.5,  3,  3,  2.75, 2.5,  null]);    
     
     // I
-    this._dins.push([null,4.5,4,3.5,3.5,3]);
+    this._dins.push([null,  null, 4.5,  4,  3.5,  3.5,  3, null]);
 
     // J
-    this._dins.push([null,5.5,5,4.5,4,3.5]);
+    this._dins.push([null,  null, 5.5,  5,  4.5,  4,  3.5,  3]);
 
     // K
-    this._dins.push([null,6.5,6,5.5,5,4.5]);
+    this._dins.push([null,  null, 6.5, 6,  5.5,  5,  4.5,  4]);
 
     // L 
-    this._dins.push([null,7.5,7,6.5,6,5.5]);
+    this._dins.push([null,  null, 7.5,  7,  6.5,  6,  5.5,  5]);
 
     // M
-    this._dins.push([null,null,8.5,8,7,6.5]);
+    this._dins.push([null,  null,  null,  8.5,  8,  7,  6.5,  6]);
 
     // N 
-    this._dins.push([null,null,10,9.5,8.5,8]);
+    this._dins.push([null,  null,  null,  10, 9.5,  8.5,  8,  7.5]);
 
     // 0
-    this._dins.push([null,null,11.5,11,10,9.5]);
+    this._dins.push([null,  null,  null,  11.5, 11, 10, 9.5,  9]);
 
     // P
-    this._dins.push([null,null,null,null,12, 11.5]);
+    this._dins.push([null,  null, null, null, null, 12, 11.5, 10.5]);
   }
     
 }
